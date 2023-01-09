2cf21fad1dfa33fdfff812544fbffb337cf783ce
EC2 Default User <ec2-user@ip-172-31-41-70.ap-northeast-1.compute.internal>
2023-01-09 15:29:55 +0000

    finish4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
-  get 'lists/show'
+  get 'lists/:id' => 'lists#show',as: 'list'
   get 'lists/edit'
   get '/top' => 'homrs#top'
   get 'lists' => 'lists#index'
